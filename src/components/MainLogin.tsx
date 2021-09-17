e0e62acf5a8276582b4ef98ee5128dfe98586472
Isaiah <computerblender@gmail.com>
2021-09-16 23:27:44 -0700

    Extend useFetch hook for POSTs; extend Login component
    Login component:
    - add controlled inputs to form
    - make post request to server
    - log response in useEffect
    
    Not currently *storing* the result, but cookies are set.

--- src/components/MainLogin.tsx
+++ src/components/MainLogin.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
+import useFetch from '../hooks/useFetch';
 import '../styles/MainLogin.scss';
 
 export default function MainLogin() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { callFetch, response } = useFetch(
+    'http://localhost:3000/api/auth/login',
+    {
+      credentials: 'include',
+    },
+  );
+
+  useEffect(() => {
+    response?.json().then(console.log).catch();
+  }, [response]);
+
+  function handleFormSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    callFetch({ username, password });
+  }
+
   return (
     <div className="login-container">
-      <form className="form login-form">
+      <form className="form login-form" onSubmit={handleFormSubmit}>
         <label>
           <div>Username:</div>
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </label>
         <label>
           <div>Password:</div>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <button className="Button" type="submit">
           Login
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 
 // TODO: caching
 export default function useFetch(
   url: string,
   options = {},
 ): {
+  callFetch: (body?: any) => {};
   isLoading: boolean;
   isError: boolean;
   response: any;
@@ -14,8 +15,17 @@ export default function useFetch(
   const [isError, setIsError] = useState(false);
   const [response, setResponse] = useState<null | Response>(null);
 
-  useEffect(() => {
-    fetch(url, options).then((response) => {
+  const callFetch = useCallback((body) => {
+    console.log('body', body);
+    const full_options = body
+      ? {
+          ...options,
+          body: JSON.stringify(body),
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+        }
+      : options;
+    fetch(url, full_options).then((response) => {
       setIsLoading(false);
 
       if (!response.ok) {
@@ -30,5 +40,5 @@ export default function useFetch(
     };
   }, []);
 
-  return { isLoading, isError, response };
+  return { callFetch, isLoading, isError, response };
 }
