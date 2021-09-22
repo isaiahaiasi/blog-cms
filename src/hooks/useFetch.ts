2772d3b6bb3adb984c35d93ac0d1ce8c6d7a1362
Isaiah <computerblender@gmail.com>
2021-09-21 21:37:03 -0700

    Add cleanup to useFetch hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -14,36 +14,45 @@ const useFetch: UseFetchInterface = function (url, options = {}) {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [response, setResponse] = useState<null | Response>(null);
+  // Possibly derivative of response.body, but that's returned in a one-time Stream,
+  // and I want to cache it without overriding/extending the response object
+  const [body, setBody] = useState<null | Record<string, any> | string>(null);
 
   const callFetch = useCallback(
-    (body) => {
-      console.log('body', body);
-      const full_options = body
+    async (body) => {
+      // Controller to abort fetch on cleanup if necessary
+      const abortController = new AbortController();
+
+      // Add body & appropriate POST headers
+      const full_options: RequestInit = body
         ? {
             ...options,
             body: JSON.stringify(body),
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
+            signal: abortController.signal,
           }
         : options;
-      fetch(url, full_options).then((response) => {
-        setIsLoading(false);
 
-        if (!response.ok) {
-          setIsError(true);
-        }
+      const res = await fetch(url, full_options);
 
-        setResponse(response);
-      });
+      setIsLoading(false);
+
+      if (!res.ok) {
+        setIsError(true);
+      }
+
+      setResponse(response);
 
       return () => {
-        // ?
+        console.log('Abandoning fetch request');
+        abortController?.abort();
       };
     },
     [url],
   );
 
-  return { callFetch, isLoading, isError, response };
+  return { callFetch, isLoading, isError, response, body };
 };
 
 export default useFetch;
