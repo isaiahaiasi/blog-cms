8441c9cbef6966d4d512ecf583ad5524f645fbdb
Isaiah <computerblender@gmail.com>
2021-09-26 22:21:43 -0700

    Add initial (ugly) Post blog feature

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,12 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { PostsContext } from '../contexts/Posts';
+import UserContext from '../contexts/user';
+import useAuthFetch from '../hooks/useAuthFetch';
 import useDefinedContext from '../hooks/useDefinedContext';
 import '../styles/Editor.scss';
 
+// TODO: extract to proper file
+function getPostBlogAPIEndpoint(userId: string, postId?: string) {
+  if (postId) {
+    throw new Error('Updating posts not implemented yet!!!');
+  } else {
+    return `http://localhost:3000/api/users/${userId}/blogs`;
+  }
+}
+
 export default function Editor() {
   const { postid } = useParams<{ postid: string }>();
-  const { posts } = useDefinedContext(PostsContext);
+  const [user] = useDefinedContext(UserContext);
+
+  const { posts, dispatch } = useDefinedContext(PostsContext);
   const activePost = posts.find((post) => post._id === postid);
 
   const [postTitle, setPostTitle] = useState('Loading');
@@ -17,6 +30,27 @@ export default function Editor() {
     setPostContent(activePost?.content ?? 'Loading');
   }, [postid]);
 
+  const [isSending, setIsSending] = useState(false);
+  const { callFetch, isLoading } = useAuthFetch(
+    getPostBlogAPIEndpoint(user?._id ?? 'undefined'),
+  );
+  useEffect(() => {
+    if (isSending) {
+      callFetch({
+        title: postTitle,
+        content: postContent,
+        publishDate: new Date(),
+      });
+    }
+  }, [isSending]);
+
+  useEffect(() => {
+    if (isLoading === false) {
+      console.log('sending complete!');
+      setIsSending(false);
+    }
+  }, [isLoading]);
+
   function handleTitleUpdate(e: React.FormEvent<HTMLInputElement>) {
     setPostTitle(e.currentTarget.value);
   }
@@ -25,9 +59,14 @@ export default function Editor() {
     setPostContent(e.currentTarget.value);
   }
 
+  function publishPost(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsSending(true);
+  }
+
   return (
     <div className="EditorContainer">
-      <form className="EditorColumn">
+      <form className="EditorColumn" onSubmit={publishPost}>
         <div className="EditorToolbar">Tools n such</div>
         <div className="EditorPage">
           <input
@@ -48,7 +87,7 @@ export default function Editor() {
           <button className="Button" type="button">
             Schedule publication
           </button>
-          <button className="Button" type="button">
+          <button className="Button" type="submit">
             Publish
           </button>
         </div>
